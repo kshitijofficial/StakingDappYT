bbdaecde1c23ad0b8d86396ce03f58291b506afc
Rajat singh <104482745+RajatSinghhh@users.noreply.github.com>
2024-02-21 22:17:32 +0530

    Update ClaimReward.jsx
    Use Capital "W" When importing Web3Context from context.
    This also applicable while doing destructuring using "useContext"
    use capital "W" in Web3Context.
    
    Thanks for making such a great project

--- client/src/components/ClaimReward/ClaimReward.jsx
+++ client/src/components/ClaimReward/ClaimReward.jsx
@@ -1,11 +1,11 @@
 import { useContext} from "react";
-import web3Context from "../../context/Web3Context"
+import Web3Context from "../../context/Web3Context"
 import Button from "../Button/Button";
 import { toast } from "react-hot-toast";
 import "./ClaimReward.css"
 
 const ClaimReward = ()=>{
- const {stakingContract}=useContext(web3Context);
+ const {stakingContract}=useContext(Web3Context);
  const claimReward = async()=>{
   try{
     const transaction = await stakingContract.getReward();
